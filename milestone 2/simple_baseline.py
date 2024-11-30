745933ac302c794444db8fa51fd512e14a03972e
Sofija Milicic <sofija.milicic@gmail.com>
2024-12-01 00:09:16 +0100

    Simplest baseline: majority class classifier

---
+++ milestone 2/simple_baseline.py
@@ -0,0 +1,35 @@
+class MajorityClassClassifier:
+    def __init__(self):
+        self.majority_class = None
+
+    def fit(self, X_train_dev, y_train_dev):
+        """
+        Determines the majority class from the training labels by manually counting.
+        No need for a separate development set, therefore use concatenation.
+        """
+        count_0 = 0
+        count_1 = 0
+        
+        for label in y_train_dev:
+            if label == 0:
+                count_0 += 1
+            elif label == 1:
+                count_1 += 1
+        
+        if count_1 > count_0:
+            self.majority_class = 1
+        else:
+            self.majority_class = 0
+
+    def predict(self, X_test):
+        """
+        Predict the majority class for all input samples, no matter the content.
+        """
+        if self.majority_class is None:
+            raise ValueError("The classifier hasn't been fitted yet.")
+        
+        return [self.majority_class] * len(X_test)
+
+
+
+# class RegexClassifier: # TO BE CONTINUED...
