7a47bc453074839a2a2a284f41442ba703369737
Sofija Milicic <sofija.milicic@gmail.com>
2024-12-12 14:29:46 +0100

    tf-idf representation included; for balanced case: Vectorizer should be fit on the balanced training set

--- milestone 2/import_preprocess.py
+++ milestone 2/import_preprocess.py
@@ -1,5 +1,5 @@
 import conllu
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from scipy.sparse import vstack
 import numpy as np
 
@@ -23,6 +23,7 @@ class ImportPreprocess:
         self.X_train, self.y_train, self.S_train = None, None, None
         self.X_val, self.y_val, self.S_val = None, None, None
         self.X_test, self.y_test, self.S_test = None, None, None
+        self.X_train_balanced, self.y_train_balanced = None, None
 
     def import_train_val_test(self):
         """
@@ -57,13 +58,13 @@ class ImportPreprocess:
     def concatenate_train_val(self):
         return self.X_train + self.X_val, self.y_train + self.y_val
     
-    def create_balanced_dataset(self, X, y, n_samples=5000):
+    def create_balanced_dataset(self, n_samples=5000):
         """
         Create a balanced dataset by sampling n_samples from each class.
         """
         conversion_needed = False
-        if y[0] != 0 and y[0] != 1:
-            y = convert_labels_to_int(y)
+        if self.y_train[0] != 0 and self.y_train[0] != 1:
+            y = convert_labels_to_int(self.y_train)
             conversion_needed = True
         y = np.array(y)
         
@@ -76,31 +77,57 @@ class ImportPreprocess:
         balanced_indices = np.concatenate([sampled_class_0_indices, sampled_class_1_indices])
         np.random.shuffle(balanced_indices) 
 
-        X_balanced = [X[i] for i in balanced_indices]
+        X_balanced = [self.X_train[i] for i in balanced_indices]
         y_balanced = [y[i] for i in balanced_indices]
         
         if conversion_needed:
             y_balanced = convert_labels_to_string(y_balanced)
+
+        self.X_train_balanced, self.y_train_balanced = X_balanced, y_balanced
         
-        return X_balanced, y_balanced
     
-    def create_bow_representation(self, max_features=300):
+    def create_bow_representation(self, max_features=300, balanced=False):
         """
         Transform tokenized sentences into bag of words (BoW) representation.
         'CountVectorizer' is applied, but without any preprocessing (already done in milestone 1).
         Due to high number of unique tokens, only the 'max_features' most frequent tokens are considered.
+        If working with balanced dataset ('balanced'), 'CountVectorizer' should be fit with it. 
+        (It's important for determining 'max_features' most occuring tokens)
         """
         vectorizer = CountVectorizer(analyzer=lambda x: x, token_pattern=None, max_features=max_features)
-        X_train_bow = vectorizer.fit_transform(self.X_train)
+        if balanced:
+            X_train_bow = vectorizer.fit_transform(self.X_train_balanced)
+        else:
+            X_train_bow = vectorizer.fit_transform(self.X_train)
+
         X_val_bow = vectorizer.transform(self.X_val)
         X_test_bow = vectorizer.transform(self.X_test)
         
-        X_balanced, _ = self.create_balanced_dataset(self.X_train, self.y_train, n_samples=5000)
-        X_balanced_bow = vectorizer.transform(X_balanced)
+        # X_balanced, _ = self.create_balanced_dataset(self.X_train, self.y_train, n_samples=5000)
+        # X_balanced_bow = vectorizer.transform(X_balanced)
 
         X_train_val_bow = vstack([X_train_bow, X_val_bow]) # for final evaluation purpose
 
-        return X_train_bow, X_val_bow, X_test_bow, X_balanced_bow, X_train_val_bow, vectorizer.get_feature_names_out()
+        # return X_train_bow, X_val_bow, X_test_bow, X_balanced_bow, X_train_val_bow, vectorizer.get_feature_names_out()
+        return X_train_bow, X_val_bow, X_test_bow, X_train_val_bow, vectorizer.get_feature_names_out()
+
+
+    def create_tfidf_representation(self, max_features=300, balanced=False):
+        """
+        Transform tokenized sentences into TF-IDF representation.
+        Same explanation as in 'create_bow_representation'.
+        """
+        vectorizer = TfidfVectorizer(analyzer=lambda x: x, token_pattern=None, max_features=max_features)
+        if balanced:
+            X_train_tfidf = vectorizer.fit_transform(self.X_train_balanced)
+        else:
+            X_train_tfidf = vectorizer.fit_transform(self.X_train)
+
+        X_val_tfidf = vectorizer.transform(self.X_val)
+        X_test_tfidf = vectorizer.transform(self.X_test)
+
+        X_train_val_tfidf = vstack([X_train_tfidf, X_val_tfidf])  # for final evaluation purpose
+
+        return X_train_tfidf, X_val_tfidf, X_test_tfidf, X_train_val_tfidf, vectorizer.get_feature_names_out()
+
     
-    def pad_token_sequences(self):
-        raise NotImplementedError()
